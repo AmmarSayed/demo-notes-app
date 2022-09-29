4b6ed0a3882728b942232ef1b97b45b753f10c33
Ammar Sayed <ammarsayed1988@gmail.com>
2022-09-29 23:03:57 +0200

    Setting up our React app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import config from "./config";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+///////////
+// The AWS setup
+///////////
+//Amplify refers to Cognito as Auth, S3 as Storage, and API Gateway as API.
+//The mandatorySignIn flag for Auth is set to true because we want our users to be signed in before they can interact with our app.
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
+///////////
+// end of AWS setup
+///////////
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
